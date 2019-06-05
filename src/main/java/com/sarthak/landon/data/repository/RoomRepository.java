c67083a1e2b8962763fb1817f8eeee6f1b00fce6
Sarthak <500053513@stu.upes.ac.in>
2019-06-05 10:15:09 +0530

    First Commit

---
+++ src/main/java/com/sarthak/landon/data/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.sarthak.landon.data.repository;
+
+import com.sarthak.landon.data.entity.Room;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends CrudRepository<Room,Long> {
+    Room findByNumber(String number);
+}
